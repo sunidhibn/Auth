f5034194ef36145b4eb17bfb9404b276182926cb
Sunidhi bn <sunidhi.bn@gmail.com>
2020-09-08 09:38:07 +0530

    Custom user model

---
+++ app/core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(BaseUserManager):
+
+    def create_user(self,email,password=None, **extra_fields):
+        """creates and save user"""
+        user=self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,password):
+        """create superuser"""
+        user=self.create_user(email,password)
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self._db)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """custom user model"""
+    email=models.EmailField(max_length=200,unique=True)
+    name=models.CharField(max_length=20)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=True)
+
+    objects=UserManager()
+    USERNAME_FIELD= 'email'
